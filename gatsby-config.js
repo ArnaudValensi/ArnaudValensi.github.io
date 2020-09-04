ab3ab8de60f76ccbbdeb79a4002a3ad7c281d56b
Arnaud Valensi <arnaud.valensi@gmail.com>
2020-09-04 12:53:51 +0200

    Disable offline mode because of github 404 error

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,21 +50,22 @@ module.exports = {
       },
     },
     `gatsby-plugin-catch-links`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `GatsbyJS`,
-        short_name: `GatsbyJS`,
-        start_url: `/`,
-        background_color: `#6b37bf`,
-        theme_color: `#6b37bf`,
-        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
-        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
-        display: `standalone`,
-        icon: `src/images/icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    `gatsby-plugin-offline`,
+    // NOTE: It is causing 404 errors on github pages for some reason.
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: `GatsbyJS`,
+    //     short_name: `GatsbyJS`,
+    //     start_url: `/`,
+    //     background_color: `#6b37bf`,
+    //     theme_color: `#6b37bf`,
+    //     // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+    //     // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+    //     display: `standalone`,
+    //     icon: `src/images/icon.png`, // This path is relative to the root of the site.
+    //   },
+    // },
+    // `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
   ],
 }
