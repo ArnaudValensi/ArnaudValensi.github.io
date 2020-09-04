5c1f21962a2358951ae2100171d6273eadb8d174
Arnaud Valensi <arnaud.valensi@gmail.com>
2020-09-04 17:10:00 +0200

    Remove gatsby offline plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,22 +50,6 @@ module.exports = {
       },
     },
     `gatsby-plugin-catch-links`,
-    // NOTE: It is causing 404 errors on github pages for some reason.
-    // {
-    //   resolve: `gatsby-plugin-manifest`,
-    //   options: {
-    //     name: `GatsbyJS`,
-    //     short_name: `GatsbyJS`,
-    //     start_url: `/`,
-    //     background_color: `#6b37bf`,
-    //     theme_color: `#6b37bf`,
-    //     // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
-    //     // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
-    //     display: `standalone`,
-    //     icon: `src/images/icon.png`, // This path is relative to the root of the site.
-    //   },
-    // },
-    // `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
   ],
 }
