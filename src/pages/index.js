680d4862a54384a4e141f933a545c86a20aeb345
Arnaud Valensi <arnaud.valensi@gmail.com>
2020-09-23 17:18:42 +0200

    Add a missing import

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Layout from "../components/Layout"
 import SEO from "../components/SEO"
+import { graphql } from "gatsby"
 
 export default function Home({ data }) {
   const post = data.markdownRemark
